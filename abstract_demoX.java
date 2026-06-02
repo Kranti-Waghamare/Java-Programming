db16a55e1a354405102a6d1f3ff5f94a18a14ee8
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-02 15:01:03 +0530

    abstract clss in java

---
+++ abstract_demoX.java
@@ -0,0 +1,23 @@
+abstract class Demo
+{
+    public int i,j;
+    public int Add(int a, int b)     //Concrete Method
+    {
+        return a+b;
+    }
+
+    public abstract int Sub(int a, int b);    
+}
+
+class Hello extends Demo
+{
+
+}
+
+class abstract_demoX
+{
+    public void main(String A[])
+    {
+       Hello hobj = new Hello();
+    }
+}
