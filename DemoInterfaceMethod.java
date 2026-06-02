5ad48d71a089f3ada8d0ffd42d7f4d9128b882a8
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-02 15:09:59 +0530

    Interface method in java.

---
+++ DemoInterfaceMethod.java
@@ -0,0 +1,22 @@
+interface Circle
+{
+    //characteristics   (public static final)
+    float PI = 3.14f;           
+
+    //Behaviour      (public abstrct)
+    float Area(float Radius);
+    float Circumfarance(float Radius);
+}
+
+class Marvellous implements Circle
+{
+    Error due to missing body of area and circumfarance
+}
+
+class DemoInterfaceMethod
+{
+    public static void main(String A[])
+    {
+         Marvellous mobj = new Marvellous();
+    }
+}
